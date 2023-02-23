2f884e0d9f9f9684a96a30b5ba5317ac649bc79c
colocolov <colocolov.stas@gmail.com>
2023-02-23 16:17:01 +0200

    upd menu in mob ver and fix adaptive

--- #src/js/scripts/accordeon.js
+++ #src/js/scripts/accordeon.js
@@ -3,7 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const accordeons = document.querySelectorAll(".accordeon__item");
   const accordeonBtn = document.querySelectorAll(".accordeon__title");
   const navLink = document.querySelectorAll(".menu__item");
+  const pathname = window.location.pathname; // возвращает путь до текущей страницы
+  const href = window.location.href; // возвращает полный адрес страницы
+  const hrefLink = localStorage.getItem("hrefLink"); // записываем хеш в localStorage
 
+  // переключение табов
   accordeonBtn.forEach((el) => {
     el.addEventListener("click", (e) => {
       // console.log(el);
@@ -18,17 +22,19 @@ document.addEventListener("DOMContentLoaded", () => {
         parent.onclick = function (e) {
           this.classList.toggle("active");
           // console.log(e.target.className + "--");
-          if (e.target.className == "btn") {
-            // alert(this.className);
-            // console.log(this);
-          }
+          // if (e.target.className == "btn") {
+          //   // alert(this.className);
+          //   // console.log(this);
+          // }
         };
       }
 
       // .self.classList.toggle("active");
     });
   });
+  // END переключение табов
 
+  // закрытие мобильного меню
   navLink.forEach((item) => {
     item.addEventListener("click", () => {
       // let url = item.baseURI;
@@ -37,17 +43,36 @@ document.addEventListener("DOMContentLoaded", () => {
       // console.log(hash.toLowerCase());
 
       accordeons.forEach((el) => {
-        el.classList.remove("active");
-        // console.log(el);
+        el.classList.remove("active"); // получают элементы меню
         const isCob = el.classList.contains(hash.toLowerCase());
-        // console.log(isCob);
-        while (isCob) {
-          el.classList.add("active");
-          break;
+
+        if (pathname != '/index.html') {
+          localStorage.setItem("hrefLink", hash.toLowerCase());
+          // alert(ркуа);
+        } else {
+          while (isCob) {
+            el.classList.add("active");
+            break;
+          }
+          localStorage.removeItem('hrefLink');
         }
       });
-      // console.dir(item);
     });
   });
+  // END закрытие мобильного меню
+
+  // проверка если мы перешли с другой страницы с хешем и открываем нужную секцию
+  if (href.includes('#')) { // если мы на главной с хешем
+      for (let i = 0, parent; (parent = accordeons[i]); i++) {
+        parent.classList.remove("active");
+        if (parent.classList.contains(hrefLink)) {
+          parent.classList.add('active');
+        }
+      }
+  } else {
+    localStorage.removeItem('hrefLink');
+  }
+  // END проверки на хэш
+
 });
 // ----- END
